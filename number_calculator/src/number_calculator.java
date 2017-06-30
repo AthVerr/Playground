1194f0161b75d967ebc2fb0840032612d84c893c
Athina Verroiopoulou <athina.verroiopoulou@gmail.com>
2017-06-30 13:38:10 -0500

    Add files via upload

---
+++ number_calculator/src/number_calculator.java
@@ -0,0 +1,17 @@
+
+/**
+ *2ergasia
+ * icsd08011
+ * @author Athina Verroiopoulou
+ */
+
+public class number_calculator {
+   
+    
+      public static void main(String[] args) {
+        
+             pedio pan=new pedio();
+             pan.setSize(250,250); //diastaseis parathirou
+             pan.setVisible(true);
+    }
+}
